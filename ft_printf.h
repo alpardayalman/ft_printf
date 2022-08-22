bea91053e39ecd5ad41e7a8c68bf92fc1044a18f
Alp arda Yalman <ayalman@k2m07s06.42kocaeli.com.tr>
2022-08-22 23:47:58 +0300

    printf

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayalman <ayalman@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 23:08:28 by ayalman           #+#    #+#             */
+/*   Updated: 2022/08/22 23:42:30 by ayalman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_format(va_list *args, char c);
+int	ft_int(int a);
+int	ft_hex(unsigned int a, char c);
+int	ft_point(unsigned long a, int sign);
+int	ft_string(char *str);
+int	ft_unsigned(unsigned int a);
+
+#endif
